6bc01c0630912f52d98aa2e767387313f533d890
Bryan Burke <bburke@scrippsnetworks.com>
2016-05-26 16:05:05 -0400

    Reverted to default keys bindings
    - Additionally tweaked the right prompt to not display the edit mode

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,25 +1,11 @@
 function fish_right_prompt
-	switch "$fish_bind_mode"
-        case "default"
-            set_color yellow
-            echo -n command
-        case "insert"
-            set_color --bold green
-            echo -n insert
-        case "visual"
-            set_color magenta
-            echo -n visual
-    end
-    set_color normal
-
-    # Print the checked out branch name or commit hash of a git repository.
+	# Print the checked out branch name or commit hash of a git repository.
     set -l current_commit (git rev-parse --short HEAD 2> /dev/null)
 
     if test $status -gt 0
         return
     end
 
-    echo -n ":"
     git diff-files --quiet --ignore-submodules 2>/dev/null
 
     if test $status = 0
--- fish/functions/fish_user_key_bindings.fish
+++
@@ -1,7 +0,0 @@
-function fish_user_key_bindings
-	fish_vi_key_bindings
-    # Custom key-bindings
-    bind -M insert \cf forward-char
-    bind -M insert \eb backward-word
-    bind -M insert \cl 'clear; commandline -f repaint'
-end
