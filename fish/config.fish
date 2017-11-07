5b0e0fc3652d90631c38b895aa678a0a466d4171
Bryan Burke <bburke@scrippsnetworks.com>
2017-11-07 09:23:55 -0500

    Updating gpg-agent command to honor config file

--- fish/config.fish
+++ fish/config.fish
@@ -28,7 +28,7 @@ set -xg CLICOLOR true
 set -xg LSCOLORS "exfxFxBxcxbxdxabagacad"
 set -xg GPG_TTY (tty)
 
-pgrep -x gpg-agent >/dev/null; or gpg-agent --daemon -c --pinentry-program /opt/local/bin/pinentry-curses
+pgrep -x gpg-agent >/dev/null; or gpg-agent --daemon -c
 
 if test "$FISH_BBURKE_STARTAGENT" = "true"
   test -t 1; and startagent
